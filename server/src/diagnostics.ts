e847143d726a524fcd84ab706337b56a243a4dbe
bscan <10503608+bscan@users.noreply.github.com>
2022-05-02 18:56:44 -0400

    Use nqp for syntax checking instead of raku -c

--- server/src/diagnostics.ts
+++ server/src/diagnostics.ts
@@ -15,19 +15,22 @@ import {
 } from 'vscode-languageserver-textdocument';
 
 export async function rakucompile(textDocument: TextDocument, workspaceFolders: WorkspaceFolder[] | null, settings: NavigatorSettings): Promise<CompilationResults | void> {
-    let rakuParams: string[] = ["-c"];
+    
+    const navigatorPath = join(dirname(__dirname), 'src', 'raku', 'navigator.raku');
+
+    let rakuParams: string[] = [navigatorPath];
     const filePath = Uri.parse(textDocument.uri).fsPath;
 
     rakuParams = rakuParams.concat(getIncPaths(workspaceFolders, settings));
-    nLog("Starting raku compilation check with the equivalent of: " + settings.rakuPath + " " + rakuParams.join(" ") + " " + filePath, settings);
+    nLog("Starting raku compilation check with " + settings.rakuPath + " " + rakuParams.join(" ") + " " + filePath, settings);
 
     let output: string;
     let stdout: string;
-    let severity: DiagnosticSeverity;
     const diagnostics: Diagnostic[] = [];
     const code = textDocument.getText();
     let myenv = process.env;
 	myenv.RAKUDO_ERROR_COLOR = '0';
+
     try {
         const process = async_execFile(settings.rakuPath, rakuParams, {timeout: 10000, maxBuffer: 20 * 1024 * 1024});
         process?.child?.stdin?.on('error', (error: any) => { 
@@ -40,13 +43,11 @@ export async function rakucompile(textDocument: TextDocument, workspaceFolders: 
 
         output = out.stderr;
         stdout = out.stdout;
-        severity = DiagnosticSeverity.Warning;
     } catch(error: any) {
         // TODO: Check if we overflowed the buffer.
         if("stderr" in error && "stdout" in error){
             output = error.stderr;
             stdout = error.stdout;
-            severity = DiagnosticSeverity.Error;
         } else {
             nLog("rakucompile failed with unknown error", settings);
             nLog(error, settings);
@@ -54,66 +55,76 @@ export async function rakucompile(textDocument: TextDocument, workspaceFolders: 
         }
     }
 
-    nLog("Raku compilation results", settings);
-    nLog(output, settings);
+    // nLog("Raku compilation results", settings);
+    // nLog(output, settings);
+    // nLog("Raku stdout results", settings);
+    // nLog(stdout, settings);
 
+    if(stdout){
+        parseFromRaku(stdout, diagnostics); // Errors are dropped into stdout
+    }
     if(output){
-        nLog("Calling maybeAddCompDiag", settings);
-
-        maybeAddCompDiag(output, severity, diagnostics, filePath);
+        parseUnhandled(output, diagnostics);
     }
     return {diags: diagnostics};
 }
 
 
-function maybeAddCompDiag(violation: string, severity: DiagnosticSeverity , diagnostics: Diagnostic[], filePath: string): void {
+// Most errors and warnings are caught in navigator.raku and piped back here for parsing. 
+function parseFromRaku(stdout: string, diagnostics: Diagnostic[]): void {
 
-    violation = violation.replace(/\r/g, ""); // Clean up for Windows
-    violation = violation.replace(/, <STDIN> line 1\.$/g, ""); // Remove our stdin nonsense
-    violation = violation.replace("===SORRY!=== Error while compiling -\n", "");   
-    violation = violation.replace("===SORRY!===\n", "");
+    stdout = stdout.replace(/\r/g, ""); // Clean up for Windows
 
-    const lineNum = localizeErrors(violation, filePath);
-    if (typeof lineNum == 'undefined') return;
+    let match: RegExpExecArray | null;
+    const violations = stdout.split("~||~");
+    
+    violations.forEach(violation => {
 
-    diagnostics.push({
-        severity: severity,
-        range: {
-            start: { line: lineNum, character: 0 },
-            end: { line: lineNum, character: 500 }
-        },
-        message: "Syntax: " + violation,
-        source: 'raku navigator'
+        if(match = /^(\d+)~\|~(\d+)~\|~(.+)/s.exec(violation)){
+            let lineNum = +match[1] - 1;
+            let message = match[3];
+            let bError = +match[2];
+            let severity: DiagnosticSeverity;
+            if(lineNum < 0) lineNum = 0;
+            if(bError){
+                severity = DiagnosticSeverity.Error;
+            } else {
+                severity = DiagnosticSeverity.Warning;
+            }
+            diagnostics.push({
+                severity: severity,
+                range: {
+                    start: { line: lineNum, character: 0 },
+                    end: { line: lineNum, character: 500 }
+                },
+                message: "Syntax: " + message,
+                source: 'raku navigator'
+            });
+        }
     });
 }
 
 
-function localizeErrors (violation: string, filePath: string): number | void {
+// Some warnings are not currently caught within Raku and leak into STDERR. Let's catch them here and categorize them
+function parseUnhandled(violations: string, diagnostics: Diagnostic[]): void {
+    violations = violations.replace(/\r/g, ""); // Clean up for Windows
 
-
-
-    console.log("Trying to match ");
-    console.log(violation)
     let match: RegExpExecArray | null;
+    let lineNum: number;
+    var re =  /^\s+(.+?)\n\s+at.+\:(\d+)$/gm;
+    while(match = re.exec(violations)){
+        lineNum = +match[2] - 1;
+        let message = match[1];
+        if(lineNum < 0) lineNum = 0;
 
-    if(match = /\nat (.*?):(\d+)\n------>/i.exec(violation)){
-        if(match[1] == '-'){
-            return +match[2] - 1;
-        } else {
-            console.log("Did not match filename");
-            console.log(match[1]);
-            // The error/warnings must be in an imported library (possibly indirectly imported)
-            return 0; 
-        }
+        diagnostics.push({
+            severity: DiagnosticSeverity.Warning,
+            range: {
+                start: { line: lineNum, character: 0 },
+                end: { line: lineNum, character: 500 }
+            },
+            message: "Warning: " + message,
+            source: 'raku navigator'
+        });
     }
-
-    if(match = /Could not find \S+ at line (\d+) in:/i.exec(violation)){
-        return +match[1] - 1;
-    }
-    
-    if(match = /Undeclared (?:routine|name)s?:\n\s*\S+ used at line (\d+)/i.exec(violation)){
-        return +match[1] - 1;
-    }
-    
-    return 0;
 }
---
+++ server/src/raku/navigator.raku
@@ -0,0 +1,108 @@
+use nqp;
+
+# Migrated from See https://github.com/Raku/Raku-Parser/blob/master/lib/Perl6/Parser.pm6
+
+say "Running navigator.raku";
+
+my $code = $*IN.slurp;
+
+# say "Received code: $code\n";
+
+sub parse-code( Str $source ) {
+	my $*LINEPOSCACHE;
+	
+	my $compiler := nqp::getcomp('perl6');
+
+	if (!$compiler) {
+		$compiler := nqp::getcomp('Raku');
+	}
+
+	my $g := nqp::findmethod(
+		$compiler,'parsegrammar'
+	)($compiler);
+
+	my $a := nqp::findmethod(
+		$compiler,'parseactions'
+	)($compiler);
+
+	# Safer way to parse
+	my $munged-source = $source;
+	$munged-source ~~ s:g{ 'BEGIN' } = 'ENTER';
+	$munged-source ~~ s:g{ 'CHECK' } = 'ENTER';
+
+	my $parsed = $g.parse(
+		$munged-source,
+		:p( 0 ),
+		:actions( $a )
+	);
+
+	$parsed;
+}
+
+my $parsed;
+
+try {
+	# Warnings are not always capture in @worries. How do we get those?
+  $parsed = parse-code($code);
+}
+
+if ($!) {
+	# We check a number of fields hunting for the error
+	# If we can't find it, we'll drop the gist of $! back at the first line
+	my $bFoundError = 0; 
+
+	if ($!.can('sorrows')) {
+		$bFoundError = 1;
+		for $!.sorrows -> $sorrow {
+			format-exc($sorrow, 1);
+		}
+	}
+
+	if ($!.can('panic')) {
+		$bFoundError = 1;
+		format-exc($!.panic, 1);
+	}
+
+	if ($!.can('worries')) {
+		$bFoundError = 1;
+		for $!.worries -> $worry {
+			format-exc($worry, 0);
+		}
+	}
+
+	if ($!.can('message') and $!.can('line')) {
+		$bFoundError = 1;
+		print-exc($!.message, $!.line, 1);
+	}
+	
+	if ($bFoundError == 0) {
+		say "Could not figure out the error structure of" ~ $!.WHO;
+		my $message = $!.gist();
+		print-exc($message, 0, 1);
+	}
+	say "~||~"; # Terminate final exception. Probably not needed
+	exit(1);
+} 
+
+sub format-exc ($exc, $level) {
+	if ($exc.can('message') and $exc.can('line') ) { # and $exc.can('filename')
+		print-exc($exc.message, $exc.line, $level);
+	} else {
+		my $message = $exc.gist();
+		print-exc($message, 0, $level);
+	}
+}
+
+sub print-exc($message, $line, $level) {
+	my $resolvedLine = $line;
+
+	# Errors from some types appear to be located at the wrong spots, or I didn't find the correct attribute for $line
+	if $message ~~ m:s/used at lines? (\d+)/ {
+		$resolvedLine = $0;
+	}
+	if $message ~~ m:s/Could not find \S+ at line (\d+) in/ {
+		$resolvedLine = $0;
+	}
+
+	say "~||~" ~ $resolvedLine ~ "~|~" ~ $level ~ "~|~" ~ $message;
+}
